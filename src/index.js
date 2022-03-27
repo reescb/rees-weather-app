bf9641be8c01ef8bad419b9996a2f94787c28e46
reescb <rees.cb@googlemail.com>
2022-03-27 10:02:04 +0100

    Updated

--- src/index.js
+++ src/index.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 //display weather position
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f0b1594042f8bed99c56223ceb9a1125";
   let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
